8c27a748717db7650fde2c7418eb95e3c6d1f45a
tetration <rafael.august.b@gmail.com>
2022-06-04 18:05:17 -0300

    Added logic to calculate all classes grade

--- src/.grade_Calculator.js
+++ src/.grade_Calculator.js
@@ -1,6 +1,27 @@
 var teste = "";
 var collection = '';
 
+function GetHowManyClasses() {//Gets how many classes the user is taking
+  var conntainerOfClasses = document.querySelector("body > p:nth-child(1) > table > tbody > tr > td > center > table > tbody > tr:nth-child(4) > td:nth-child(1) > p > table > tbody > tr:nth-child(2) > td > p > span");
+  var NumberofClassesUserIsTaking = conntainerOfClasses.getElementsByTagName('table').length / 2;
+  return NumberofClassesUserIsTaking;
+}
+
+function printSubjectsAndGrade() {
+  console.log("You are currently enrolled in the following subjects");
+  var numofClasses = GetHowManyClasses();
+  childCounter = 1
+  childCounter2=2;
+  for (let i = 0; i < numofClasses; i++) {
+    let elementLocation = `body > p:nth-child(1) > table > tbody > tr > td > center > table > tbody > tr:nth-child(4) > td:nth-child(1) > p > table > tbody > tr:nth-child(2) > td > p > span > table:nth-child(${childCounter}) > tbody > tr > td:nth-child(1) > p`;
+    let collegeClassName=document.querySelector(elementLocation).innerText;
+    collegeClassName.replace('\n','');
+    console.log(collegeClassName);
+    getTotalClassGrade(childCounter2,collegeClassName);
+    childCounter += 2;
+    childCounter2+=3;
+  }
+}
 function turnIntoAnArrayOfNumbers(arrayOfStrings) {
   var counter = 0;
   newArray = new Array();
@@ -19,24 +40,27 @@ function SumArray(arrayOfNumbers) {
   });
   return theSum;
 }
+function getTotalClassGrade(trNthChildValue, collegeClassName) {
+  
+  let elementLocation = `body > p:nth-child(1) > table > tbody > tr > td > center > table > tbody > tr:nth-child(4) > td:nth-child(1) > p > table > tbody > tr:nth-child(2) > td > p > span > table:nth-child(${trNthChildValue}) > tbody > tr > td:nth-child(4)`;
+  $(elementLocation).each(function () {
+    //console.log($(this).html());
+    teste += $(this).html();
+    teste = teste.replace(/<[^>]*>?/gm, '');//strips html
+    teste = teste.replace(/\([^()]*\)/g, '');//strips parenthesis and everything inside it
+    teste = teste.replace('&nbsp;', '');//&nbsp;¨
 
-$('body > p:nth-child(1) > table > tbody > tr > td > center > table > tbody > tr:nth-child(4) > td:nth-child(1) > p > table > tbody > tr:nth-child(2) > td > p > span > table:nth-child(10) > tbody > tr > td:nth-child(4)').each(function () {
-  //console.log($(this).html());
-  teste += $(this).html();
-  teste = teste.replace(/<[^>]*>?/gm, '');//strips html
-  teste = teste.replace(/\([^()]*\)/g, '');//strips parenthesis and everything inside it
-  teste = teste.replace('&nbsp;', '');//&nbsp;¨
+  });
 
-});
-//colllection = teste.split('\n');
-//console.log("The collection is:" + collection);
-teste = teste.replace(/[^ 0-9\.]+/g, "");//removes words
-teste = teste.replace(/  +/g, ' ');
-collection = teste.split(' ');
-var onlyNumbers = teste.replace(/\D/g, '');
-console.log(teste);
-console.log("-------------------------------------------------------------------------------");
-collection= collection.filter(n => n)//removes null, emtpy and undefined from array
-numberCollection = turnIntoAnArrayOfNumbers(collection);
-console.log("The final sum of your grades is: " + SumArray(numberCollection));
+  teste = teste.replace(/[^ 0-9\.]+/g, "");//removes words
+  teste = teste.replace(/  +/g, ' ');
+  collection = teste.split(' ');
+  var onlyNumbers = teste.replace(/\D/g, '');
+  console.log(teste);
+  console.log("-------------------------------------------------------------------------------");
+  collection = collection.filter(n => n)//removes null, emtpy and undefined from array
+  numberCollection = turnIntoAnArrayOfNumbers(collection);
+  console.log(`The final sum of grade in ${collegeClassName}  is: ` + SumArray(numberCollection));
+}
+printSubjectsAndGrade();
 
